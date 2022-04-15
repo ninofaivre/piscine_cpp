8b76eec7848566e90011fe738a72aff3a2db6354
nino <nino.faivre@netc.fr>
2022-04-15 18:33:15 +0200

    repair split

--- ex01/srcs/PhoneBook.cpp
+++ ex01/srcs/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: nfaivre <nfaivre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:18:14 by nfaivre           #+#    #+#             */
-/*   Updated: 2022/04/15 17:02:37 by nfaivre          ###   ########.fr       */
+/*   Updated: 2022/04/15 18:32:53 by nfaivre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	PhoneBook::add(void)
 {
 	if (this->amount >= this->max_amount)
 	{
-		std::cout << "! You reached the max amount of Contact !" << std::endl;
+		std::cout << "! You reached the max amount of Contact !" << std::endl
 				  << "# The oldest contact will be replaced by the new one #" << std::endl;
 	}
 	this->Contacts[this->amount % this->max_amount].set_fields_value();
@@ -46,11 +46,11 @@ bool	PhoneBook::is_valid_search_index(std::string buffer, int amount)
 
 void	PhoneBook::display_search_header(void)
 {
-	std::cout << std::string ((4 * Contact::preview_column_width) + 7, '-') << std::endl;
+	std::cout << std::string ((4 * Contact::preview_column_width) + 7, '-') << std::endl
 			  << "||" << std::setw(Contact::preview_column_width) << "index" << '|';
 	for (int i = Contact::first_name; i <= Contact::nickname; i++)
 		std::cout << std::setw(Contact::preview_column_width) << Contact::fields_name[i] << '|';
-	std::cout << '|' << std::endl;
+	std::cout << '|' << std::endl
 			  << std::string ((4 * Contact::preview_column_width) + 7, '-') << std::endl;
 }
 
@@ -70,7 +70,7 @@ void	PhoneBook::search(void) const
 	while (!is_valid_search_index(buffer, this->amount))
 	{
 		std::cout << "# Type an index between 0 and "
-				  << (((this->amount < this->max_amount) ? (this->amount) : (this->max_amount)) - 1) << " #" << std::endl;
+				  << (((this->amount < this->max_amount) ? (this->amount) : (this->max_amount)) - 1) << " #" << std::endl
 				  << "> ";
 		std::getline(std::cin, buffer);
 		if (!is_valid_search_index(buffer, this->amount))
