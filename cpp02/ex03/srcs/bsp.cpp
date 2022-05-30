5f9761b268abecf1007f26d4739b4a6f25ba171a
nino <nino.faivre@netc.fr>
2022-05-30 08:24:00 +0200

    clean cpp02-03

--- cpp02/ex03/srcs/bsp.cpp
+++ cpp02/ex03/srcs/bsp.cpp
@@ -6,23 +6,19 @@
 /*   By: nfaivre <nfaivre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 08:39:44 by nfaivre           #+#    #+#             */
-/*   Updated: 2022/05/29 19:27:32 by nfaivre          ###   ########.fr       */
+/*   Updated: 2022/05/30 08:21:47 by nfaivre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-static Fixed	poulet(const Point &e1, const Point &e2, const Point &p)
+static Fixed	area(const Point &e1, const Point &e2, const Point &p)
 {
-	Fixed res = ((e2.getX() - p.getX()) * (e1.getY() - p.getY())) - ((e2.getY() - p.getY()) * (e1.getX() - p.getX()));
-	return (res);
+	return (Fixed (((e2.getX() - p.getX()) * (e1.getY() - p.getY())) - ((e2.getY() - p.getY()) * (e1.getX() - p.getX()))));
 }
 
 bool	bsp(const Point &a, const Point &b, const Point &c, const Point &point)
 {
-	if (poulet(a, b, point) > 0 && poulet(c, a, point) > 0 && poulet(b, c, point) > 0)
-		return (true);
-	else if (poulet(a, b, point) < 0 && poulet(c, a, point) < 0 && poulet(b, c, point) < 0)
-		return (true);
-	return (false);
+	return ((area(a, b, point) > 0 && area(c, a, point) > 0 && area(b, c, point) > 0)
+		|| (area(a, b, point) < 0 && area(c, a, point) < 0 && area(b, c, point) < 0));
 }
