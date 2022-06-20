4f4e7c35ebccc4431f231cc66a5b876ccccb1f37
nino <nino.faivre@netc.fr>
2022-06-20 18:53:57 +0200

    udpate cpp08-00 remove useless var

--- cpp08/ex00/srcs/easyfind.hpp
+++ cpp08/ex00/srcs/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: nfaivre <nfaivre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 14:01:22 by nfaivre           #+#    #+#             */
-/*   Updated: 2022/06/19 16:06:53 by nfaivre          ###   ########.fr       */
+/*   Updated: 2022/06/20 18:53:18 by nfaivre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,5 @@
 template <typename T>
 typename T::iterator	easyfind(T &big, int little)
 {
-	typename T::iterator it = find(big.begin(), big.end(), little);
-
-	return (it);
+	return (find(big.begin(), big.end(), little));
 }
